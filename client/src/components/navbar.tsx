cb3a2939d5d279ccafee3817e9dba72ea3b127d2
Bruno Hirae <brunodeluca117@outlook.com>
2025-03-19 10:57:32 -0300

    feat: add navbar component

---
+++ client/src/components/navbar.tsx
@@ -0,0 +1,64 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+import { NAVBAR_HEIGHT } from '@/lib/constants';
+
+export function Navbar() {
+  return (
+    <nav
+      className='fixed top-0 left-0 w-full z-50 shadow-xl'
+      style={{ height: `${NAVBAR_HEIGHT}px` }}
+    >
+      <div className='flex justify-between items-center w-full h-full py-3 px-8 bg-primary-700 text-white'>
+        <div className='flex items-center gap-4 md:gap-6'>
+          <Link
+            href='/'
+            className='cursor-pointer hover:!text-primary-300'
+            scroll={false}
+          >
+            <div className='flex items-center gap-3'>
+              <Image
+                src='/logo.svg'
+                alt='Rentiful Logo'
+                width={24}
+                height={24}
+                className='size-6'
+              />
+
+              <div className='text-xl font-bold'>
+                RENT
+                <span className='text-secondary-500 font-light hover:!text-primary-300'>
+                  IFUL
+                </span>
+              </div>
+            </div>
+          </Link>
+        </div>
+
+        <p className='text-primary-200 hidden md:block'>
+          Discover your perfect rental apartment with our advanced search
+        </p>
+
+        <div className='flex items-center gap-5'>
+          <Link href='/sign-in'>
+            <Button
+              variant='outline'
+              className='text-white border-white bg-transparent hover:bg-white hover:text-primary-700 rounded-lg cursor-pointer'
+            >
+              Sign In
+            </Button>
+          </Link>
+
+          <Link href='/sign-up'>
+            <Button
+              variant='outline'
+              className='text-white border-white bg-secondary-600 hover:bg-white hover:text-primary-700 rounded-lg cursor-pointer'
+            >
+              Sign Up
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
