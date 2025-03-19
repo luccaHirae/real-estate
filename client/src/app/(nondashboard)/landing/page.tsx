5d24236b4a44ae6182797da7b4cb996b6255ed74
Bruno Hirae <brunodeluca117@outlook.com>
2025-03-19 12:04:38 -0300

    feat: add discover section to landing page

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Hero } from '@/app/(nondashboard)/landing/components/hero';
 import { Features } from '@/app/(nondashboard)/landing/components/features';
+import { Discover } from '@/app/(nondashboard)/landing/components/discover';
 
 const Landing = () => {
   return (
     <>
       <Hero />
       <Features />
+      <Discover />
     </>
   );
 };
