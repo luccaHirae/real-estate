168fcceda46610d26c4d5afc540fb0f7bb34cce7
Bruno Hirae <brunodeluca117@outlook.com>
2025-03-19 11:25:19 -0300

    feat: add NonDashboardLayout component with Navbar integration

---
+++ client/src/app/(nondashboard)/layout.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NAVBAR_HEIGHT } from '@/lib/constants';
+import { Navbar } from '@/components/navbar';
+
+interface NonDashboardLayoutProps {
+  children: React.ReactNode;
+}
+
+const NonDashboardLayout = ({ children }: NonDashboardLayoutProps) => {
+  return (
+    <div className='h-full w-full'>
+      <Navbar />
+      <main
+        className='h-full w-full flex flex-col'
+        style={{ paddingTop: `${NAVBAR_HEIGHT}px` }}
+      >
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default NonDashboardLayout;
