c62c2bafe138a17cfafb79f208f3ef36a3543af0
Bruno Hirae <brunodeluca117@outlook.com>
2025-03-21 12:04:42 -0300

    feat: integrate AWS Amplify authentication with custom sign-in and sign-up components

--- client/src/app/providers.tsx
+++ client/src/app/providers.tsx
@@ -1,13 +1,21 @@
 'use client';
 
+import { Authenticator } from '@aws-amplify/ui-react';
 import StoreProvider from '@/state/redux';
+import Auth from '@/app/(auth)/auth-provider';
 
 interface ProvidersProps {
   children: React.ReactNode;
 }
 
 const Providers = ({ children }: ProvidersProps) => {
-  return <StoreProvider>{children}</StoreProvider>;
+  return (
+    <StoreProvider>
+      <Authenticator.Provider>
+        <Auth>{children}</Auth>
+      </Authenticator.Provider>
+    </StoreProvider>
+  );
 };
 
 export default Providers;
