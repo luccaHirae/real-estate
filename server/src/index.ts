617ee0dcaae90ed44414daebe4f4aa125fa69a64
Bruno Hirae <brunodeluca117@outlook.com>
2025-03-21 15:27:53 -0300

    feat: add tenant management routes and controllers for retrieving and creating tenants

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,10 @@ import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 
+import tenantRoutes from './routes/tenant-routes';
+import { authMiddleware } from './middleware/auth-middleware';
+
+/* Config */
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -15,10 +19,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
+/* Routes */
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
+app.use('/tenants', authMiddleware(['tenant']), tenantRoutes);
 
+/* Server */
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
