ebf3ef98b5a65375112619c70915cb8c93643beb
Bruno Hirae <brunodeluca117@outlook.com>
2025-04-08 12:32:29 -0300

    feat: add utility functions for cleaning parameters and formatting price values

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -4,3 +4,28 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function cleanParams(params: Record<string, any>) {
+  return Object.fromEntries(
+    Object.entries(params).filter(
+      ([, value]) =>
+        value !== undefined &&
+        value !== 'any' &&
+        value !== '' &&
+        (Array.isArray(value) ? value.some((v) => v !== null) : value !== null)
+    )
+  );
+}
+
+export function formatPriceValue(value: number | null, isMin: boolean) {
+  if (value === null || value === 0)
+    return isMin ? 'Any Min Price' : 'Any Max Price';
+
+  if (value >= 1000) {
+    const kValue = value / 1000;
+    return isMin ? `$${kValue}K+` : `<$${kValue}K`;
+  }
+
+  return isMin ? `$${value}+` : `<$${value}`;
+}
