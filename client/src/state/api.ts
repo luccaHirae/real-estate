a53c1532b3a87bbb60f21b1eae2512fe5c86d272
Bruno Hirae <brunodeluca117@outlook.com>
2025-04-09 14:59:41 -0300

    feat: add getProperties endpoint for property retrieval with filters

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,7 +1,9 @@
 import { fetchAuthSession, getCurrentUser } from 'aws-amplify/auth';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { insertNewUser } from '@/lib/handlers';
-import { Manager, Tenant } from '@/types/prisma';
+import { Manager, Property, Tenant } from '@/types/prisma';
+import { cleanParams } from '@/lib/utils';
+import { FiltersState } from '.';
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
@@ -18,7 +20,7 @@ export const api = createApi({
     },
   }),
   reducerPath: 'api',
-  tagTypes: ['Tenant', 'Manager'],
+  tagTypes: ['Tenant', 'Manager', 'Properties'],
   endpoints: (build) => ({
     getAuthUser: build.query<User, void>({
       queryFn: async (_, _queryApi, _extraOptions, fetchWithBQ) => {
@@ -88,6 +90,39 @@ export const api = createApi({
       }),
       invalidatesTags: (result) => [{ type: 'Manager', id: result?.id }],
     }),
+    // property endpoints
+    getProperties: build.query<
+      Property[],
+      Partial<FiltersState & { favoriteIds?: number[] }>
+    >({
+      query: (filters) => {
+        const params = cleanParams({
+          location: filters.location,
+          priceMin: filters.priceRange?.[0],
+          priceMax: filters.priceRange?.[1],
+          beds: filters.beds,
+          baths: filters.baths,
+          propertyType: filters.propertyType,
+          squareFeetMin: filters.squareFeet?.[0],
+          squareFeetMax: filters.squareFeet?.[1],
+          amenities: filters.amenities?.join(','),
+          latitude: filters.coordinates?.[1],
+          longitude: filters.coordinates?.[0],
+        });
+
+        return {
+          url: 'properties',
+          params,
+        };
+      },
+      providesTags: (result) =>
+        result
+          ? [
+              ...result.map(({ id }) => ({ type: 'Properties' as const, id })),
+              { type: 'Properties', id: 'LIST' },
+            ]
+          : [{ type: 'Properties', id: 'LIST' }],
+    }),
   }),
 });
 
@@ -95,4 +130,5 @@ export const {
   useGetAuthUserQuery,
   useUpdateTenantSettingsMutation,
   useUpdateManagerSettingsMutation,
+  useGetPropertiesQuery,
 } = api;
