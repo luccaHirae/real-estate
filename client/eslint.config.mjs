70069db93d59966ff4105e3447c243ef7d1f0822
Bruno Hirae <brunodeluca117@outlook.com>
2025-04-28 12:07:04 -0300

    chore: update ESLint configuration to include additional TypeScript rules

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-empty-object-type': 'warn',
+      '@typescript-eslint/no-unnecessary-type-constraint': 'warn',
+      '@typescript-eslint/no-wrapper-object-types': 'warn',
+      '@typescript-eslint/no-unsafe-function-type': 'warn',
+    }
+  }
 ];
 
 export default eslintConfig;
